5b827b79727e6017d62380053a9bbc7bc0436dc4
manuvi03 <manuvi03@ucm.es>
2024-11-07 23:40:15 +0100

    Inserto la librería necesaria para la autentificación de los usuarios

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.database)
+    implementation(libs.firebase.auth)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
