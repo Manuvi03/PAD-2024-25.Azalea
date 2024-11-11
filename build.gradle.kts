5398e5e739f07f15cef7f1e180e4230df329ffc2
daniji09 <daniji09@ucm.es>
2024-11-11 21:44:59 +0100

    Interacción con BD, MUY PROTOTIPO. Solo para saber como va. Cambios que habría que implementar: organizarlo según MVVM, hacer el readAll bien, transacciones...

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+
+    // dependencia para el plugin de Gradle para los servicios de Google
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
