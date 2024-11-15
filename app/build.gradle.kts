0cfa54998bf7b19daddf2dd79d5268634fd9e333
jorgep6 <jorgep11@ucm.es>
2024-11-15 17:02:02 +0100

    Cambios para la implementación del chat.
    Añadidos la implementation de messaging de firebase en el gradle y el servicio del mismo en el mannifest.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,10 +32,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    dataBinding{
+    dataBinding {
         enable = true
     }
-    viewBinding{
+    viewBinding {
         enable = true;
     }
 }
@@ -50,7 +50,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.messaging)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
