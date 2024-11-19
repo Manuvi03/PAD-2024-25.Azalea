12921f09a130a20293486c3511131c16a43ceaf8
Hugo de Benito <hdeben01@ucm.es>
2024-11-19 14:03:29 +0100

    cambios en el userModel y createTeacherUseCase

--- app/src/main/java/es/ucm/fdi/azalea/integration/createTeacherUseCase.java
+++ app/src/main/java/es/ucm/fdi/azalea/integration/createTeacherUseCase.java
@@ -1,4 +1,10 @@
 package es.ucm.fdi.azalea.integration;
 
+import es.ucm.fdi.azalea.business.model.UserModel;
+
 public class createTeacherUseCase {
+
+    public void createTeacher(UserModel data, CallBack<Boolean> cb){
+
+    }
 }
