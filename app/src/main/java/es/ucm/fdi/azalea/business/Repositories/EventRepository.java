83b27420fbecfc118173e0b9d01079e3edcaa14b
anonymous <116196853+DenisaJuarranzBerindea@users.noreply.github.com>
2024-11-20 15:37:36 +0100

    Merge con develop

--- app/src/main/java/es/ucm/fdi/azalea/business/Repositories/EventRepository.java
+++ app/src/main/java/es/ucm/fdi/azalea/business/Repositories/EventRepository.java
@@ -1,4 +1,14 @@
 package es.ucm.fdi.azalea.business.Repositories;
 
+import java.util.List;
+
+import es.ucm.fdi.azalea.business.model.EventModel;
+import es.ucm.fdi.azalea.integration.CallBack;
+
+import com.google.android.gms.tasks.Task;
+
+
 public interface EventRepository {
+    public void create(EventModel em, CallBack<List<EventModel>> cb);
+
 }
--- app/src/main/java/es/ucm/fdi/azalea/business/Repositories/implementations/EventRepositoryImp.java
+++ app/src/main/java/es/ucm/fdi/azalea/business/Repositories/implementations/EventRepositoryImp.java
@@ -1,7 +1,15 @@
 package es.ucm.fdi.azalea.business.Repositories.implementations;
 
+import java.util.List;
+
 import es.ucm.fdi.azalea.business.Repositories.EventRepository;
+import es.ucm.fdi.azalea.business.model.EventModel;
+import es.ucm.fdi.azalea.integration.CallBack;
 
 public class EventRepositoryImp implements EventRepository {
 
+    @Override
+    public void create(EventModel em, CallBack<List<EventModel>> cb) {
+
+    }
 }
